a5bb8dfa743396933c094bef369fdf8c6987b92a
ulibdev <ulisesbdeveloper@gmail.com>
2024-08-14 23:08:53 -0700

    Add files via upload

---
+++ calculadora.cpp
@@ -0,0 +1,20 @@
+#include "calculadora.h"
+
+calculadora::calculadora() {}
+
+float calculadora::sumar(float a, float b) {
+	return a + b;
+}
+
+float calculadora::restar(float a, float b){
+	return a - b;
+}
+
+float calculadora::multiplicar(float a, float b) {
+	return a * b;
+}
+
+float calculadora::dividir(float a, float b) {
+	return a / b;
+}
+
