5bc00ed38b25e0ac8e06b3a829c2cf7c268ecf83
ulibdev <ulisesbdeveloper@gmail.com>
2024-08-18 23:48:40 -0700

    I learned object-oriented programming in C#

---
+++ Calculator in c#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = Reading.Read();
+            int b = Reading.Read();
+
+            Calculate.Plus(a, b);
+
+            Calculate.Minus(a, b);
+
+            Calculate.Multiply(a, b);
+
+            Calculate.Divide(a, b);
+
+        }
+    }
+}
